d766f0ff5de9e6e27265f4b2f417cbdf16314022
nestorcabrera <49215548+nestorcabrera@users.noreply.github.com>
2024-09-24 18:36:54 -0500

    Add .gitignore

--- BuberDinner.Api/obj/Debug/net8.0/BuberDinner.Api.AssemblyInfo.cs
+++ BuberDinner.Api/obj/Debug/net8.0/BuberDinner.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberDinner.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8001d70c787361eb1e8908aca890dfc7de03df9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuberDinner.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuberDinner.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
